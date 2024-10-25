82e5740cbe36b3b0ba99332fcb09dc2ee45fc46a
Mate Mišlov <matemislov90@gmail.com>
2024-10-25 15:48:18 +0200

    Testing variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ export default async function Home() {
   return (
     <main className="py-20 px-[5%]">
       <div className="container mx-auto">
-        <h1 className="text-4xl font-bold">Hello {process.env.WEBSITE_NAME}</h1>
+        <h1 className="text-4xl font-bold">
+          Server code: {process.env.WEBSITE_NAME}
+        </h1>
+        <h3 className="text-2xl font-bold">
+          Client code: {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+        </h3>
         <p className="text-lg">This is a test</p>
       </div>
     </main>
